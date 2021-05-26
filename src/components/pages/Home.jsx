ed9056d0ce57c4a5366eb377d60c5873b805970d
Nikolay <dmitriev9229@mail.ru>
2021-05-26 09:12:33 +0300

    # 4 sorting
    fix arrow sort

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = React.memo(() => {
     });
 
     const [sortDirection, setDirection] = React.useState(false);
-    const [activeSort, setSort] = React.useState(false);
+    const [typeSort, setTypeSort] = React.useState('');
 
     const dispatch = useDispatch();
 
@@ -49,9 +49,9 @@ const Home = React.memo(() => {
                 default: return false;
             };
         });
-        setSort(!activeSort);
-        dispatch(setUsers(sortableUsers));
+        setTypeSort(type);
         setDirection(!sortDirection);
+        dispatch(setUsers(sortableUsers));
     };
 
     const viewMoreInfo = (user) => {
@@ -68,8 +68,8 @@ const Home = React.memo(() => {
 
     return (
         <div className="container">
-            <button onClick={  }>SMALL DATA</button>
-            <button onClick={  }>BIG DATA</button>
+            {/* <button onClick={  }>SMALL DATA</button>
+            <button onClick={  }>BIG DATA</button> */}
             {
                 isLoaded && 
                 <>
@@ -90,18 +90,21 @@ const Home = React.memo(() => {
                                         theadArr.map(({ name, id }) => {
                                             return (
                                                 <th
-                                                    // className="content__table__strings--head"
-                                                    className= {activeSort ? 'ascending' : 'descending'}
+                                                    className="content__table__strings--head"
                                                     key={ `${ id }_${ name }` }
                                                     onClick={ () => sortUsers(name) }
                                                 >
                                                     { name }
-                                                    {/* { activeSort && <svg className={ sortDirection ? '' : 'rotate' } width="10" height="6" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 
-                                                        5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 
-                                                        4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 
-                                                        0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"/>
-                                                    </svg>} */}
+                                                    {
+                                                        typeSort === name
+                                                            ? <svg className={ sortDirection ? '' : 'rotate' } width="10" height="6" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
+                                                                <path d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 
+                                                                5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 
+                                                                4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 
+                                                                0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"/>
+                                                            </svg>
+                                                            : null
+                                                    }
                                                 </th>
                                             )
                                         })
