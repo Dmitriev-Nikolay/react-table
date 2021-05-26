2f1933f34bb65446d05433f2950873de66bdccb9
Nikolay <dmitriev9229@mail.ru>
2021-05-26 15:31:25 +0300

    # 6.1 fake api

--- src/components/ToogleData.jsx
+++ src/components/ToogleData.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ToogleData = ({ getSmallData, getBigData }) => {
 
-    const API_SMALL = `http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`;
-    const API_BIG = `http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`;
+    const API_SMALL = `https://raw.githubusercontent.com/Dmitriev-Nikolay/fake-api-for-projects/main/fake-json/react-table/small_data.json`;
+    const API_BIG = `https://raw.githubusercontent.com/Dmitriev-Nikolay/fake-api-for-projects/main/fake-json/react-table/big_data.json`;
 
     const getSmall = () => {
         getSmallData(API_SMALL);
